3aded2e29383677c3978831257eaa9694212c474
chandan kashyap <chandankashyap7479@gmail.com>
2026-05-30 23:13:54 +0530

    BEan

--- Task_Manager/src/main/java/com/Task_Manager/config/AppConfig.java
+++ Task_Manager/src/main/java/com/Task_Manager/config/AppConfig.java
@@ -1,15 +1,17 @@
 package com.Task_Manager.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.Bean;
+
 
 @Configuration
 public class AppConfig {
+	
+	@Bean
+	public PasswordEncoder passwordEncoded() {
+		return new BCryptPasswordEncoder();
+	}
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
